2f0527c9fa3a0fde643a3d06243f8c140a9b2dee
EllenMarques <ellen.marques@wikidados.com.br>
2024-03-06 14:10:58 -0300

    created database

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const fs = require ('fs')
 const app = express()
 
 app.get('/', (request, response) => {
     return response.send('Hello, world!')
+})
+
+app.get('/todos', (request, response) => {
+    fs.readFile('./store/todos.json', 'utf-8')
 })
 
 app.listen(3000, () => {
